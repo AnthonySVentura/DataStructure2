7f88f564e6f448c1fe647271a1c0a658c9a7645d
AnthonySVentura <107430112+AnthonySVentura@users.noreply.github.com>
2022-07-05 08:09:12 -0400

    Finished

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("What word would you like to reverse?");
+string word = Console.ReadLine();
+   
+
+static string Reverse(string word)
+{
+    Stack<string> stack = new Stack<string>();
+
+    stack.Push(word);
+
+    return stack.Pop();
+
+}
